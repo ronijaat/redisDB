5c62e3fb189b181b9cd7ad5688e6db7096fae4a3
raunakbaliyan101 <raunak.baliyan@zenithholidays.com>
2026-03-13 11:54:34 +0530

    ping

--- core/resp.go
+++ core/resp.go
@@ -1,6 +1,9 @@
 package core
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 func readSimpleString(data []byte) (string, int, error) {
 	pos := 1
@@ -80,10 +83,35 @@ func DecodeOne(data []byte) (interface{}, int, error) {
 	return nil, 0, nil
 }
 
+func DecodeArrayString(data []byte) ([]string, error) {
+	value, err := Decode(data)
+	if err != nil {
+		return nil, err
+	}
+	ts := value.([]interface{})
+	tokens := make([]string, len(ts))
+	for i := range ts {
+		tokens[i] = ts[i].(string)
+	}
+
+	return tokens, nil
+}
+
 func Decode(data []byte) (interface{}, error) {
 	if len(data) == 0 {
 		return nil, errors.New("no data")
 	}
 	value, _, err := DecodeOne(data)
 	return value, err
+}
+
+func Encode(value interface{}, isSimple bool) []byte {
+	switch v := value.(type) {
+	case string:
+		if isSimple {
+			return []byte(fmt.Sprintf("+%s\r\n", v))
+		}
+		return []byte(fmt.Sprintf("$%d\r\n%s\r\n", len(v), v))
+	}
+	return []byte{}
 }
