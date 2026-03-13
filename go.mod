421e8645921d3ecd03a9b1415da14bf238cddb31
raunakbaliyan101 <raunak.baliyan@zenithholidays.com>
2026-03-13 10:32:24 +0530

    simple tcp server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ronijaat/redisDB
+
+go 1.23
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"flag"
+	"log"
+
+	"github.com/ronijaat/redisDB/config"
+	"github.com/ronijaat/redisDB/server"
+)
+
+func setupFlags() {
+	flag.StringVar(&config.HOST, "host", "0.0.0.0", "host for the redis server")
+	flag.IntVar(&config.PORT, "port", 7379, "port for redis db server")
+	flag.Parse()
+}
+func main() {
+	setupFlags()
+	log.Println("rolling the redis")
+	server.RunSyncTCPServer()
+}
