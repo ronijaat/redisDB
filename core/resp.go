861e25244a0b363b81d1fe5f1ed7f8b1b507be17
raunakbaliyan101 <raunak.baliyan@zenithholidays.com>
2026-03-16 14:42:54 +0530

    `Added support for SET, GET, and TTL Redis commands`

--- core/resp.go
+++ core/resp.go
@@ -112,6 +112,8 @@ func Encode(value interface{}, isSimple bool) []byte {
 			return []byte(fmt.Sprintf("+%s\r\n", v))
 		}
 		return []byte(fmt.Sprintf("$%d\r\n%s\r\n", len(v), v))
+	case int64:
+		return []byte(fmt.Sprintf(":%d\r\n", value))
 	}
 	return []byte{}
 }
